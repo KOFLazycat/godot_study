f3bed06fd8f5de8a90a797c373984dbb7ef19720
shoulei <shoulei@staff.weibo.com>
2022-10-28 16:26:39 +0800

    1. 增加场景

--- demos/CottonPuzzle/globals/SceneChanger.gd
+++ demos/CottonPuzzle/globals/SceneChanger.gd
@@ -1,7 +1,17 @@
-extends Node
-@onready var animation_player = $AnimationPlayer
+extends CanvasLayer
+@onready var color_rect = $ColorRect
+
+func _ready():
+	pass
+	#print(color_rect)
 
 func change_scene(path: String):
-	animation_player.play("fade-in")
-	await animation_player.animation_finished
-	get_tree().change_scene_to_file(path)
+	pass
+	#var tween = create_tween()
+	#tween.tween_callback(color_rect.show)
+	#tween.tween_property(color_rect, "color:a", 1.0, 0.2)
+	#tween.tween_callback(get_tree().change_scene.bind(path))
+	#tween.tween_property(color_rect, "color:a", 0.0, 0.3)
+	#tween.tween_callback(color_rect.hide)
+	
+

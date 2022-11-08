b09642c36d866676b002168178be325999ff1ed0
shoulei <shoulei@staff.weibo.com>
2022-11-08 16:19:17 +0800

    1. rts game 初始化

---
+++ demos/RTS YT GAME/scenes/ui/Camera.gd
@@ -0,0 +1,52 @@
+extends Camera2D
+
+var mousePos = Vector2()
+var mousePosGlobal = Vector2()
+var start = Vector2()
+var startV = Vector2()
+var end = Vector2()
+var endV = Vector2()
+var isDragging = false
+signal area_selected
+signal start_move_selection
+
+@onready var panel = $"../Panel"
+
+
+func _process(delta):
+	if Input.is_action_just_pressed("LeftClick"):
+		start = mousePosGlobal
+		startV = mousePos
+		isDragging = true
+		
+	if isDragging:
+		end = mousePosGlobal
+		endV = mousePos
+		draw_area()
+		
+	if Input.is_action_just_released("LeftClick"):
+		if startV.distance_to(mousePos) > 20:
+			end = mousePosGlobal
+			endV = mousePos
+			isDragging = false
+			draw_area(false)
+			emit_signal("area_selected")
+		else:
+			end = start
+			isDragging = false
+			draw_area(false)
+	
+	
+func _input(event):
+	if event is InputEventMouse:
+		mousePos = event.position
+		mousePosGlobal = get_global_mouse_position()
+
+
+func draw_area(s = true):
+	panel.size = Vector2(abs(startV.x - endV.x), abs(startV.y - endV.y))
+	var pos = Vector2()
+	pos.x = min(startV.x, endV.x)
+	pos.y = min(startV.y, endV.y)
+	panel.position = pos
+	panel.size *= int(s)

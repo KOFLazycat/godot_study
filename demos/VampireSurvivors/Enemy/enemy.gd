9c1bd26c84389dc4d1aadcf8e31d289b34c46a80
KOFLAzycat <lushoulei0552@163.com>
2022-11-17 21:04:29 +0800

    1. 增加敌人 2. 增加动画

---
+++ demos/VampireSurvivors/Enemy/enemy.gd
@@ -0,0 +1,24 @@
+extends CharacterBody2D
+
+
+@export var movement_speed = 20.0
+
+@onready var player = get_tree().get_first_node_in_group("player")
+@onready var animation_player = $AnimationPlayer
+@onready var sprite_2d = $Sprite2D
+
+
+func _ready():
+	animation_player.play("enemy_kolbold_weak_walk")
+
+
+func _physics_process(_delta):
+	var direction = global_position.direction_to(player.global_position)
+	# 增加余量可以让怪物转向增加一点反应时间，看起来相对真实
+	if direction.x > 0.1:
+		sprite_2d.flip_h = true
+	if direction.x < -0.1:
+		sprite_2d.flip_h = false
+	
+	velocity = direction*movement_speed
+	move_and_slide()
--- demos/VampireSurvivors/Player/player.gd
+++ demos/VampireSurvivors/Player/player.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 
 var movement_speed = 40.0
+@onready var sprite_2d = $Sprite2D
+@onready var animation_player = $AnimationPlayer
 
 
 func _physics_process(delta):
@@ -12,5 +14,16 @@ func movement():
 	var x_mov = Input.get_action_strength("right") - Input.get_action_strength("left")
 	var y_mov = Input.get_action_strength("down") - Input.get_action_strength("up")
 	var mov = Vector2(x_mov, y_mov)
+	
+	if mov.x > 0:
+		sprite_2d.flip_h = true
+	if mov.x < 0:
+		sprite_2d.flip_h = false
+	
+	if mov == Vector2.ZERO:
+		animation_player.stop()
+	else:
+		animation_player.play("player_walk")
+	
 	velocity = mov.normalized() * movement_speed
 	move_and_slide()

5c7c1ff31e3f0cdc790bb93dfcd32ae0a8399965
koflazycat <lushoulei0552@163.com>
2023-10-17 17:00:34 +0800

    1. 增加sword动画

--- demos/2DSurvivorsCourse/src/main/scene/role/basic_enemy/basic_enemy.gd
+++ demos/2DSurvivorsCourse/src/main/scene/role/basic_enemy/basic_enemy.gd
@@ -10,7 +10,7 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	var direction: Vector2 = get_direction_to_player()
 	velocity = direction * MAX_SPEED
 	move_and_slide()
--- demos/2DSurvivorsCourse/src/main/scene/role/player/player.gd
+++ demos/2DSurvivorsCourse/src/main/scene/role/player/player.gd
@@ -10,14 +10,13 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	var direction: Vector2 = get_movement_vector()
 	velocity = direction * MAX_SPEED
 	move_and_slide()
 
 
 func get_movement_vector() -> Vector2:
-	var movement_vector: Vector2 = Vector2.ZERO
 	var x_movement: float = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	var y_movement: float = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	return Vector2(x_movement, y_movement).normalized()

dfc2b852735e7ac88bc9fa3c6e950e7a0cc4510a
koflazycat <lushoulei0552@163.com>
2023-10-17 15:08:54 +0800

    1. 增加2D幸存者游戏课程项目

---
+++ demos/2DSurvivorsCourse/src/main/scene/game_camera/game_camera.gd
@@ -0,0 +1,21 @@
+extends Camera2D
+
+
+var target_position: Vector2 = Vector2.ZERO
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	make_current()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	acquire_target()
+	global_position = global_position.lerp(target_position, 1.0 - exp(-delta * 10))
+
+
+func acquire_target() -> void:
+	var player: CharacterBody2D = get_tree().get_first_node_in_group("player")
+	if is_instance_valid(player):
+		target_position = player.global_position
---
+++ demos/2DSurvivorsCourse/src/main/scene/role/player/player.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+
+const MAX_SPEED: float = 200.0
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var direction: Vector2 = get_movement_vector()
+	velocity = direction * MAX_SPEED
+	move_and_slide()
+
+
+func get_movement_vector() -> Vector2:
+	var movement_vector: Vector2 = Vector2.ZERO
+	var x_movement: float = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	var y_movement: float = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	return Vector2(x_movement, y_movement).normalized()

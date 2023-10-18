2466a4451b3261b97f11b3f8c962228efe1fade8
KOFLAzycat <lushoulei0552@163.com>
2023-10-19 00:20:22 +0800

    1. 增加消灭敌人逻辑

--- demos/2DSurvivorsCourse/src/main/scene/ability/sword_ability_controller/sword_ability_controller.gd
+++ demos/2DSurvivorsCourse/src/main/scene/ability/sword_ability_controller/sword_ability_controller.gd
@@ -2,6 +2,7 @@ extends Node
 
 
 @export var sword_ability: PackedScene
+@export var max_range: float = 150
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,5 +14,23 @@ func on_timer_timeout() -> void:
 	var sword_insance: Node2D = sword_ability.instantiate() as Node2D
 	if !is_instance_valid(player) or !is_instance_valid(sword_insance):
 		return
+		
+	var enemies = get_tree().get_nodes_in_group("enemy")
+	enemies = enemies.filter(func(enemy: Node2D):
+		return enemy.global_position.distance_squared_to(player.global_position) <= pow(max_range, 2)
+	)
+	if enemies.size() <= 0:
+		return
+	
+	enemies.sort_custom(func (a: Node2D, b: Node2D):
+		var a_distance: float = a.global_position.distance_squared_to(player.global_position)
+		var b_distance: float = b.global_position.distance_squared_to(player.global_position)
+		return a_distance < b_distance
+	)
+	
 	player.get_parent().add_child(sword_insance)
-	sword_insance.global_position = player.global_position
+	sword_insance.global_position = enemies[0].global_position
+	sword_insance.global_position += Vector2.RIGHT.rotated(randf_range(0, TAU)) * 4
+	
+	var enemy_direction: Vector2 = enemies[0].global_position - sword_insance.global_position
+	sword_insance.rotation = enemy_direction.angle()
--- demos/2DSurvivorsCourse/src/main/scene/role/basic_enemy/basic_enemy.gd
+++ demos/2DSurvivorsCourse/src/main/scene/role/basic_enemy/basic_enemy.gd
@@ -6,7 +6,7 @@ const MAX_SPEED: float = 75
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$Area2D.area_entered.connect(on_area_entered)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -21,3 +21,7 @@ func get_direction_to_player() -> Vector2:
 	if is_instance_valid(player):
 		return (player.global_position - global_position).normalized()
 	return Vector2.ZERO
+
+
+func on_area_entered(area: Area2D) -> void:
+	queue_free()

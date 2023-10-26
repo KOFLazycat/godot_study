b6ce39eef1bb282686616fc336158e39e7255fe4
KOFLAzycat <lushoulei0552@163.com>
2023-10-26 15:03:51 +0800

    1. 增加字体 2. 增加伤害数字动画

--- demos/2DSurvivorsCourse/src/main/scene/component/hurtbox_component.gd
+++ demos/2DSurvivorsCourse/src/main/scene/component/hurtbox_component.gd
@@ -3,6 +3,7 @@ extends Area2D
 
 @export var health_component: Node
 
+var floating_text_scene: PackedScene = preload("res://src/main/scene/ui/floating_text.tscn")
 
 func _ready() -> void:
 	area_entered.connect(on_area_entered)
@@ -15,3 +16,8 @@ func on_area_entered(area: Area2D) -> void:
 		return
 	var hitbox_component = area as HitboxComponent
 	health_component.damage(hitbox_component.damage)
+
+	var floating_text_instance = floating_text_scene.instantiate()
+	get_tree().get_first_node_in_group("foreground_layer").add_child(floating_text_instance)
+	floating_text_instance.global_position = global_position + Vector2.UP * 8
+	floating_text_instance.start(str(hitbox_component.damage))
--- demos/2DSurvivorsCourse/src/main/scene/game_object/experience_vial.gd
+++ demos/2DSurvivorsCourse/src/main/scene/game_object/experience_vial.gd
@@ -1,11 +1,39 @@
 extends Node2D
 
+@onready var collision_shape_2d: CollisionShape2D = $Area2D/CollisionShape2D
+@onready var sprite_2d: Sprite2D = $Sprite2D
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$Area2D.area_entered.connect(on_area_entered)
 
 
-func on_area_entered(_area: Area2D) -> void:
+func tween_collect(percent: float, start_position: Vector2) -> void:
+	var player = get_tree().get_first_node_in_group("player") as Node2D
+	if !is_instance_valid(player):
+		return
+	global_position = start_position.lerp(player.global_position, percent)
+	var direction_from_start: Vector2 = player.global_position - start_position
+	
+	var target_rotation = direction_from_start.angle() + deg_to_rad(90)
+	rotation = lerp_angle(rotation, target_rotation, 1.0 - exp(-5 * get_process_delta_time()))
+
+
+func collect() -> void:
 	GameEvents.emit_experience_vial_collected(1)
 	queue_free()
+
+
+func on_area_entered(_area: Area2D) -> void:
+	var tween = create_tween()
+	# 后面的tween并行进行
+	tween.set_parallel(true)
+	# tween 指定方法
+	tween.tween_method(tween_collect.bind(global_position), 0.0, 1.0, 0.5).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_IN)
+	# tween_property指定属性变化，并设置延迟
+	tween.tween_property(sprite_2d, "scale", Vector2.ZERO, 0.15).set_delay(0.35)
+	# 结束并行，后面的会串行
+	tween.chain()
+	tween.tween_callback(collect)
+	collision_shape_2d.set_deferred("disabled", true)

80ca4f1c05d8fb37de2a0d88cccec17f87d53796
KOFLAzycat <lushoulei0552@163.com>
2023-10-27 19:50:27 +0800

    1. 升级卡片增加动画

--- demos/2DSurvivorsCourse/src/main/scene/ui/ability_upgrade_card.gd
+++ demos/2DSurvivorsCourse/src/main/scene/ui/ability_upgrade_card.gd
@@ -4,10 +4,15 @@ signal selected
 
 @onready var name_label: Label = %NameLabel
 @onready var description_label: Label = %DescriptionLabel
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var hover_animation_player: AnimationPlayer = $HoverAnimationPlayer
+
+var disabled: bool = false
 
 
 func _ready() -> void:
 	gui_input.connect(on_gui_input)
+	mouse_entered.connect(on_mouse_entered)
 
 
 func set_ability_upgrade(upgrade: AbilityUpgrade) -> void:
@@ -15,6 +20,37 @@ func set_ability_upgrade(upgrade: AbilityUpgrade) -> void:
 	description_label.text = upgrade.description
 
 
+func play_in(delay: float = 0) -> void:
+	modulate = Color.TRANSPARENT
+	await get_tree().create_timer(delay).timeout
+	animation_player.play("in")
+
+
+func play_discard() -> void:
+	animation_player.play("discard")
+
+
+func select_card() -> void:
+	disabled = true
+	animation_player.play("selected")
+	
+	for card in get_tree().get_nodes_in_group("upgrade_card"):
+		if card == self:
+			continue
+		card.play_discard()
+	
+	await animation_player.animation_finished
+	selected.emit()
+
+
 func on_gui_input(event: InputEvent) -> void:
+	if disabled:
+		return
 	if event.is_action_pressed("left_click"):
-		selected.emit()
+		select_card()
+
+
+func on_mouse_entered() -> void:
+	if disabled:
+		return
+	hover_animation_player.play("hover")
--- demos/2DSurvivorsCourse/src/main/scene/ui/upgrade_scene.gd
+++ demos/2DSurvivorsCourse/src/main/scene/ui/upgrade_scene.gd
@@ -4,6 +4,7 @@ signal upgrade_selected(upgrade: AbilityUpgrade)
 
 @export var upgrade_card_scene: PackedScene
 @onready var card_container: HBoxContainer = %CardContainer
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 
 func _ready() -> void:
@@ -11,14 +12,19 @@ func _ready() -> void:
 
 
 func set_ability_upgrades(upgrades: Array[AbilityUpgrade]) -> void:
+	var delay: float = 0
 	for upgrade in upgrades:
 		var card_instance = upgrade_card_scene.instantiate()
 		card_container.add_child(card_instance)
 		card_instance.set_ability_upgrade(upgrade)
+		card_instance.play_in(delay)
 		card_instance.selected.connect(on_upgrade_scene_selected.bind(upgrade))
+		delay += 0.2
 
 
 func on_upgrade_scene_selected(upgrade: AbilityUpgrade) -> void:
 	upgrade_selected.emit(upgrade)
+	animation_player.play("out")
+	await animation_player.animation_finished
 	queue_free()
 	get_tree().paused = false

6d1de31f8427ef889880a21c7514b33cd7b6c57e
KOFLAzycat <lushoulei0552@163.com>
2023-11-15 21:19:09 +0800

    1. game_over界面增加动画

--- demos/GameJuiceBreakout/scenes/ui/game_over/scripts/game_over.gd
+++ demos/GameJuiceBreakout/scenes/ui/game_over/scripts/game_over.gd
@@ -2,8 +2,17 @@ extends Control
 
 signal retry()
 
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+
 func _ready() -> void:
+	animation_player.animation_finished.connect(on_animation_player_animation_finished)
 	$VBoxContainer/RetryBtn.grab_focus()
+
+
+func screen_shake(duration: float, frequency: float, amplitude: float) -> void:
+	Globals.camera.shake(duration, frequency, amplitude)
+
 
 func _on_RetryBtn_pressed() -> void:
 	emit_signal("retry")
@@ -11,3 +20,8 @@ func _on_RetryBtn_pressed() -> void:
 
 func _on_QuitBtn_pressed() -> void:
 	get_tree().quit()
+
+
+func on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "appear":
+		animation_player.play("wiggle")

a02bcab498d26b49b96d98ff2d1ab68c83ca1404
koflazycat <lushoulei0552@163.com>
2024-03-29 19:22:00 +0800

    1. 增加闪电链特效

--- demos/tweendemo/src/main/scene/level/world/world.gd
+++ demos/tweendemo/src/main/scene/level/world/world.gd
@@ -1,11 +1,13 @@
 extends Node2D
 
 @onready var lightning_beam: RayCast2D = $LightningBeam
+@onready var laser: Laser = $Laser
 
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("shoot"):
 		lightning_beam.initial_lightning()
+		laser.is_casting = not laser.is_casting
 
 
 func _process(delta: float) -> void:

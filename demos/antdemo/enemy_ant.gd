e59e85ba7b35e3d25382d53d35b0fcf44bea8c3a
KOFLAzycat <lushoulei0552@163.com>
2024-06-16 08:45:02 +0800

    1. 优化碰撞逻辑

--- demos/antdemo/enemy_ant.gd
+++ demos/antdemo/enemy_ant.gd
@@ -18,6 +18,9 @@ func _physics_process(delta: float) -> void:
 	var direction: Vector2 = to_local(navigation_agent_2d.get_next_path_position()).normalized()
 	rotation += atan2(direction.x, -direction.y) * delta
 	navigation_agent_2d.velocity = Vector2(sin(rotation), -cos(rotation)) * speed
+	#velocity = Vector2(sin(rotation), -cos(rotation)) * speed
+	#move_and_slide()
+	#animated_sprite_2d.play("default")
 
 
 func _on_timer_timeout() -> void:
